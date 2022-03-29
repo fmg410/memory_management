bfdf21d9bcc93866ff5b48ef772bef960977ed09
fmg410 <fmg4@wp.pl>
2022-03-29 23:20:46 +0200

    Example5a

--- example5.cpp
+++ example5.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <chrono>
+#include <memory>
 
 struct Partner {};
 
@@ -22,26 +23,21 @@ bool processSecond()
 
 void convertMe()
 {
-    Partner* pFirst = new Partner();
+    std::unique_ptr<Partner> pFirst(new Partner);
 
     if (!process())
     {
-        delete pFirst;
         return;
     }
 
-    Partner* pSecond = new Partner();
+    std::unique_ptr<Partner> pSecond(new Partner());
 
     if (!processSecond())
     {
-        delete pFirst;
-        delete pSecond;
         return;
     }
 
     process();
-    delete pFirst;
-    delete pSecond;
 }
 
 int main()
